7111b8385b97141d2ced838c4bd157536585eca7
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-06-27 18:32:05 +0100

    refactorization

--- bmi/src/main/java/com/bmi/IMCCalculator.java
+++ bmi/src/main/java/com/bmi/IMCCalculator.java
@@ -1,30 +1,38 @@
 package com.bmi;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IMCCalculator {
+
+    private static final Map<Double, String> weightCategories = new HashMap<>();
+    
+    static {
+        weightCategories.put(16.0, "Delgadez severa");
+        weightCategories.put(17.0, "Delgadez moderada");
+        weightCategories.put(18.5, "Delgadez leve");
+        weightCategories.put(25.0, "Peso normal");
+        weightCategories.put(30.0, "Sobrepeso");
+        weightCategories.put(35.0, "Obesidad leve");
+        weightCategories.put(40.0, "Obesidad moderada");
+    }
     
     public double calculateBMI(double weight, double height) {
         double heightSquared = Math.pow(height, 2);
         return weight / heightSquared;
     }
     
-    public String determineWeightCategory(double weight, double height) {
-        double bmi = calculateBMI(weight, height);
-         if (bmi < 16) {
-            return "Delgadez severa";
-        } else if (bmi >= 16 && bmi < 17) {
-            return "Delgadez moderada";
-        } else if (bmi >= 17 && bmi < 18.5) {
-            return "Delgadez leve";
-        } else if (bmi >= 18.5 && bmi < 25) {
-            return "Peso normal";
-        } else if (bmi >= 25 && bmi < 30) {
-            return "Sobrepeso";
-        } else if (bmi >= 30 && bmi < 35) {
-            return "Obesidad leve";
-        } else if (bmi >= 35 && bmi < 40) {
-            return "Obesidad moderada";
-        } else {
-            return "Obesidad morbida";
-        }
-    }
+   public String determineWeightCategory(double weight, double height) {
+    double bmi = calculateBMI(weight, height);
+    
+    return (bmi < 16) ? "Delgadez severa" :
+           (bmi < 17) ? "Delgadez moderada" :
+           (bmi < 18.5) ? "Delgadez leve" :
+           (bmi < 25) ? "Peso normal" :
+           (bmi < 30) ? "Sobrepeso" :
+           (bmi < 35) ? "Obesidad leve" :
+           (bmi < 40) ? "Obesidad moderada" : "Obesidad morbida";
+}
+
+
 }
