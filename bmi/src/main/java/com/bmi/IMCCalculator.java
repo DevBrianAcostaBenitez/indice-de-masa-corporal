c487e99f5d5d695598b631811c562a173df19797
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-06-27 18:40:39 +0100

    remove unnecesary code

--- bmi/src/main/java/com/bmi/IMCCalculator.java
+++ bmi/src/main/java/com/bmi/IMCCalculator.java
@@ -5,18 +5,7 @@ import java.util.Map;
 
 public class IMCCalculator {
 
-    private static final Map<Double, String> weightCategories = new HashMap<>();
-    
-    static {
-        weightCategories.put(16.0, "Delgadez severa");
-        weightCategories.put(17.0, "Delgadez moderada");
-        weightCategories.put(18.5, "Delgadez leve");
-        weightCategories.put(25.0, "Peso normal");
-        weightCategories.put(30.0, "Sobrepeso");
-        weightCategories.put(35.0, "Obesidad leve");
-        weightCategories.put(40.0, "Obesidad moderada");
-    }
-    
+   
     public double calculateBMI(double weight, double height) {
         double heightSquared = Math.pow(height, 2);
         return weight / heightSquared;
